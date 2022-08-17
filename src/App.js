46dfe46f0fcbac0c781b32191ca459d12b68685a
Mykhailo Tovtyn <mykhailo_tovtyn@epam.com>
2022-08-17 13:06:33 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import {Header} from './components/layout/Header'
+import {Footer} from './components/layout/Footer'
+import {Main} from './components/layout/Main'
+function App() {
+  return (
+  <>
+    <Header />
+    <Main />
+    <Footer />
+    
+  </>
+  );
+}
+
+export default App;
---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+return       <footer class="page-footer green lighten-5">
+
+<div className="footer-copyright">
+  <div className="container">
+  © {new Date().getFullYear()} Copyright Text
+  <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
+  </div>
+</div>
+</footer>
+}
+
+export {Footer}
